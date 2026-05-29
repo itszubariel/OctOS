708ec9c1da8a653933d2e3a1022438d8be287a84
aram-j <randomacc.for1randomthings@gmail.com>
2026-05-29 22:18:06 +0300

    chore: format kernel.c

--- kernel/kernel/kernel.c
+++ kernel/kernel/kernel.c
@@ -3,13 +3,13 @@
 #define MAGIC 0x36d76289
 
 void kernel_main(uint32_t magic, uint32_t info) {
-  if (magic != MAGIC) {
-    while (1) {
-      __asm__ volatile("hlt");
+    if (magic != MAGIC) {
+        while (1) {
+            __asm__ volatile("hlt");
+        }
     }
-  }
 
-  while (1) {
-    __asm__ volatile("hlt");
-  }
+    while (1) {
+        __asm__ volatile("hlt");
+    }
 }
